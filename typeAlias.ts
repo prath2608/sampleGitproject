4db916d73a1f0e533a1609dff7b7c13d15ed28ee
prath2608 <prathmeshrs99@gmail.com>
2025-06-12 12:18:24 +0530

    first commit

---
+++ typeAlias.ts
@@ -0,0 +1,7 @@
+/* A type alias in TypeScript allows you to give a 
+custom name to a type, making your code more readable and reusable.*/
+
+
+
+type Username = string;
+let user: Username = "Prathmesh";
